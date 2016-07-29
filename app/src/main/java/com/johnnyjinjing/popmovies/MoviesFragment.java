ded7093e3db6f1527ca9861927c473121ad85585
Jing Jin <johnnyjinjing@gmail.com>
2016-07-28 22:29:39 -0500

    add the movie db api key

--- app/src/main/java/com/johnnyjinjing/popmovies/MoviesFragment.java
+++ app/src/main/java/com/johnnyjinjing/popmovies/MoviesFragment.java
@@ -16,9 +16,9 @@ public class MoviesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
         ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
 
         Picasso.with(getContext())
