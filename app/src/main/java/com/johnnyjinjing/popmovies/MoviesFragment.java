917e0211294015650c8f50a0ee9b167d41d1f0bc
Jing Jin <johnnyjinjing@gmail.com>
2016-07-28 23:28:44 -0500

    add async task to get data from db

--- app/src/main/java/com/johnnyjinjing/popmovies/MoviesFragment.java
+++ app/src/main/java/com/johnnyjinjing/popmovies/MoviesFragment.java
@@ -1,16 +1,26 @@
 package com.johnnyjinjing.popmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MoviesFragment extends Fragment {
+
+    private TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,12 +29,95 @@ public class MoviesFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
 
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        GetMoviesTask getMoviesTask = new GetMoviesTask();
+        getMoviesTask.execute();
 
-        Picasso.with(getContext())
-                .load("https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg")
-                .into(imageView);
+        textView = (TextView) rootView.findViewById(R.id.textView);
+
+//        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+//        Picasso.with(getContext())
+//                .load("https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg")
+//                .into(imageView);
         return rootView;
     }
 
+    // Get poster and info of movies from TheMovieDB
+    public class GetMoviesTask extends AsyncTask<Void, Void, String> {
+
+        // Tag for debugging
+        private final String LOG_TAG = GetMoviesTask.class.getSimpleName();
+
+        // Declare outside the try/catch so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+
+                // Construct URL for movie query
+                // https://api.themoviedb.org/3/movie/popular?api_key=API_KEY
+                final String MOVIEDB_BASE_URL =
+                        "https://api.themoviedb.org/3/movie/popular?";
+                final String API_KEY_PARAM = "api_key";
+
+                Uri uri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
+                        .appendQueryParameter(API_KEY_PARAM, BuildConfig.THE_MOVIE_DB_API_KEY)
+                        .build();
+
+                URL url = new URL(uri.toString());
+
+                // Create the request and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing received
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Add newline, easy for debugging
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Empty stream
+                    return null;
+                }
+
+                String popMoviesJsonStr = buffer.toString();
+                return popMoviesJsonStr;
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
+    }
 }
