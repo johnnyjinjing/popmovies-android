a8c606d33728f73dd1ed1ba0ac03ca6a27a05c71
Jing Jin <johnnyjinjing@gmail.com>
2016-07-28 22:03:32 -0500

    add picasso to show figures

--- app/src/main/java/com/johnnyjinjing/popmovies/MoviesFragment.java
+++ app/src/main/java/com/johnnyjinjing/popmovies/MoviesFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class MoviesFragment extends Fragment {
@@ -12,7 +15,16 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+        Picasso.with(getContext())
+                .load("https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg")
+                .into(imageView);
+        return rootView;
     }
+
 }
